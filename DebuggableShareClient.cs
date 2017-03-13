c1a65d90a0e938a12055db2f58822938331c6926
Bjorn <bjoberg@signicat.com>
2017-03-13 16:01:03 +0100

    ...

--- DebuggableShareClient.cs
+++ DebuggableShareClient.cs
@@ -1,13 +1,6 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 
 namespace ShareClientDotNet
 {
--- ShareClient.cs
+++ ShareClient.cs
@@ -6,8 +6,8 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ShareClientDotNet
 {
